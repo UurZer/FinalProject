9dd4334831cb76f2cb7ed4742e77354791a73d84
UurZer <46075903+UurZer@users.noreply.github.com>
2021-04-14 19:07:10 +0300

    11.Gün Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -29,6 +29,8 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
+            if (DateTime.Now.Hour == 17)
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductListed);
         }
 
@@ -49,7 +51,7 @@ namespace Business.Concrete
 
         public IDataResult<List<ProductDetailDto>> GetProductDetailDtos()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 17)
                 return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
 
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProducDetailDtos());
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -20,12 +20,37 @@ namespace WebAPI.Controllers
         {
             _productService = productService;
         }
-        [HttpGet]
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
             //Dependency chain -0-0-0
             var result = _productService.GetAll();
-            return result.Data;
+            if (result.Success)
+            {
+                return Ok(result);//Datayı alır
+            }
+            return BadRequest(result);//Message alır
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            //Dependency chain -0-0-0
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);//Datayı alır
+            }
+            return BadRequest(result);//Message alır
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
